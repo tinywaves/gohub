df815c63e1fb4b3ac6da3e538efeac9114c94cad
Lyle Zheng <dhzhme@gmail.com>
2025-02-16 16:55:52 +0800

    fix: use new key

--- internal/config.go
+++ internal/config.go
@@ -6,7 +6,7 @@ const (
 	MysqlDsn          = "root:root@tcp(localhost:13306)/gohub"
 	DevUrl            = "http://localhost"
 	ProdUrl           = "https://gohub.com"
-	AuthenticationKey = "AuthenticationKey"
-	EncryptionKey     = "EncryptionKey"
+	AuthenticationKey = "os5SXpAOPZCCid5seHnUx10xOHHZnVSQQ53btZKNKUOjYaqt3U3zEPIS7zXngMp8"
+	EncryptionKey     = "ArasYlndMqAOm7vf1skQQZjeIJ5DBffWuOisKwvtaNkh9Oh67FKDSgmHS15mNV3h"
 	SessionStoreName  = "gohub-session"
 )
